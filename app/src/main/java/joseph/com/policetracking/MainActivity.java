27752df3239912787e0faa3a925afa9e7bdff74f
Brian <brncherin@gmail.com>
2016-11-19 16:56:08 -0500

    Ability to track last known location and location changes

--- app/src/main/java/joseph/com/policetracking/MainActivity.java
+++ app/src/main/java/joseph/com/policetracking/MainActivity.java
@@ -1,13 +1,27 @@
 package joseph.com.policetracking;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
+    Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LocationTracker locationTracker = new LocationTracker(context);
+        locationTracker.startTrackingLocation();
+
     }
+
 }
